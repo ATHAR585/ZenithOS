0a1ae3faa676b10034be3434751acb69a983d970
ATHAR585 <cuongg.nguyenn04@gmail.com>
2026-06-08 00:41:12 +0300

    ROM_STATUS: OFFICIAL

--- unica/patches/miscs/customize.sh
+++ unica/patches/miscs/customize.sh
@@ -3,11 +3,11 @@ REGION=$(echo -n "$TARGET_FIRMWARE" | cut -d "/" -f 2)
 
 # Set build ID
 ROM_STATUS=""
-$ROM_IS_OFFICIAL || ROM_STATUS=" UNOFFICIAL"
+$ROM_IS_OFFICIAL || ROM_STATUS=" OFFICIAL"
 VALUE="$(GET_PROP "$WORK_DIR/system/system/build.prop" "ro.build.display.id")"
 SET_PROP "system" "ro.build.display.id" "ZenithOS$ROM_STATUS $ROM_CODENAME $ROM_VERSION - $TARGET_CODENAME ($VALUE)"
 
-SET_PROP "system" "ro.extremerom.official" "$ROM_IS_OFFICIAL"
+SET_PROP "system" "ro.extremerom.official" "true"
 SET_PROP "system" "ro.extremerom.version" "$ROM_VERSION"
 SET_PROP "system" "ro.extremerom.codename" "$ROM_CODENAME"
 
