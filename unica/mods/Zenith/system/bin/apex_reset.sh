892f60a2c3342af521f91c0b731620dfd87f1321
Szucsy92 <szucsy92@gmail.com>
2026-06-07 22:01:27 +0300

    unica: Mod: Zenith: Update Apex Performance

--- unica/mods/Zenith/system/bin/apex_reset.sh
+++ unica/mods/Zenith/system/bin/apex_reset.sh
@@ -27,5 +27,8 @@ sleep 10
 # Counter reset
 echo 0 > "$FILE"
 
+# PSTORE TISZTÍTÁS: Kijavítja a beragadó kernel panic hibát
+rm -f /sys/fs/pstore/* 2>/dev/null
+
 # Log
-echo "$(date '+%Y-%m-%d %H:%M:%S') | BOOT SUCCESS - COUNTER RESET" >> "$LOG_FILE"
+echo "$(date '+%Y-%m-%d %H:%M:%S') | BOOT SUCCESS - COUNTER RESET & PSTORE CLEARED" >> "$LOG_FILE"
